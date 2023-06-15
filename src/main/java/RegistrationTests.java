e1457a97e69a0864e9dc0edc66bf7169d2fd1830
VBuryshev <v.buryshev@gmail.com>
2023-06-15 20:49:42 +0300

    CW_06
    Base_of_methods_start

--- src/main/java/RegistrationTests.java
+++ src/main/java/RegistrationTests.java
@@ -2,23 +2,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class RegistrationTests
+public class RegistrationTests extends TestBase
 {
-    WebDriver wd;
-
-    @BeforeMethod
-    public void init()
-    {
-        wd = new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
+//    WebDriver wd;
+//
+//    @BeforeMethod
+//    public void init()
+//    {
+//        wd = new ChromeDriver();
+//        wd.navigate().to("https://telranedu.web.app/home");
+//        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//    }
 
 @Test
 public void registrationPositiveTest()
@@ -64,8 +65,25 @@ public void registrationPositiveTest()
 
         // click on Button Registration
         wd.findElement(By.xpath("//button[2]")).click();
+
+        //assert error message
+        //pause(5000);
+        //Assert.assertTrue(wd.findElements(By.xpath("/button")).size()>0);
+        pause(5000);
+        Assert.assertTrue(isElementPresent(By.xpath("/button")));
+
+
     }
 
+    @Test
+    public void registrationNegativeWrongEmail()
+    {
+        int i = (int)(System.currentTimeMillis()/1000)%3600;
+        String email = "testlg" + i + "mail.com", password = "Act123456";
+        openLoginForm();
+        fillLoginForm(email, password);
+        submitRegistration();
+    }
 
     @AfterMethod
     public void tearDown()
